3a4e07e88bcb6b26f00416fc3a49f6d4881a9168
chirothespearow <ritvikrrao@gmail.com>
2023-06-11 20:35:20 +0530

    Add operation and number functionality

--- script.js
+++ script.js
@@ -12,10 +12,21 @@ function multiply(a,b){
 
 function divide(a,b){
     if(b===0){
-        return 'Divide By Zero';
+        return 'Error';
     }
-    return (a/b).toFixed(6);
+    if(a%b === 0){
+        return a/b;
+    }
+    let len = (a/b + '').length;
+    if (len>9){
+        return (a/b).toFixed(6);
+    }
+    else{
+        return (a/b).toFixed(len-2);
+    }    
 }
+
+
 
 function operate(firstNumber,secondNumber,operator){
     switch(operator){
@@ -23,15 +34,63 @@ function operate(firstNumber,secondNumber,operator){
             return(add(firstNumber,secondNumber));
         case '-':
             return(subtract(firstNumber,secondNumber));
-        case '*':
+        case '×':
             return(multiply(firstNumber,secondNumber));
-        case '/':
+        case '÷':
             return(divide(firstNumber,secondNumber));
     }
 }
 // End of calculation functions
 
-let firstNumber = 0;
-let secondNumber = 0;
+let firstNumber = '';
+let secondNumber = '';
 let operator = '';
-console.log(operate(1,3,'/'));
+
+let firstInput = true;
+let currentButton = null;
+let activeOperator = false;
+// Adding buttons functionality
+
+//Number functionality
+const numbers = document.querySelectorAll('.number');
+numbers.forEach(i =>
+    i.addEventListener('click',function(e){
+        if(firstInput){
+            firstNumber+=i.textContent;
+            document.querySelector('.display').textContent = (firstNumber);
+        }
+        else{
+            secondNumber+=i.textContent;
+            document.querySelector('.display').textContent = (secondNumber);
+        }
+        
+        if(i.textContent === '.'){
+            i.disabled = true;
+        }
+    }))
+
+//Operator functionality
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach(j => j.addEventListener('click',function(e){
+    if(j.textContent === '='){
+        if(firstNumber && secondNumber && operator){
+            let display = document.querySelector('.display');
+            display.textContent = parseFloat(operate(+firstNumber,+secondNumber,operator));
+            // if error, do something
+            activeOperator= !activeOperator;
+            //
+            firstNumber = display.textContent;
+            operator = '';
+            secondNumber = '';
+            firstInput = true;
+        }
+    }
+
+    else{
+        operator = j.textContent;
+        firstInput = !firstInput;
+        currentButton = j;  //for highlighting purposes
+
+    }
+}))
